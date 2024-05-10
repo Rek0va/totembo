d6997d01ffa959ee2f383cbf587e22c317c82833
mauve <mauve@gmail.com>
2024-05-10 20:42:08 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = prompt('Введите свое имя...', 'Эвелина')
+let age = prompt ('Введите ваш возраст...')
+console.log(`Здравствуйте! ${name}, ваш возраст ${age}`)
+
+let example1 = prompt('Решите пример 150 + 150 = ?')
+console.log(`Пример 1: 150 + 150 = 300, ваш ответ: ${example1}`)
+
+let example2 = prompt('Решите пример 150 - 20 = ?')
+console.log(`Пример 2: 150 - 20 = 130, ваш ответ: ${example2}`)
+
+let example3 = prompt('Решите пример 300 / 150 = ?')
+console.log(`Пример 3: 300 / 150 = 2, ваш ответ: ${example3}`)
+
+let example4 = prompt('Решите пример 2 * 2 = ?')
+console.log(`Пример 4: 2 * 2 = 4, ваш ответ: ${example4}`)
+
+alert(`Откройте Console (F12)`)
